98c53caa8904c3f9e9edc0bf8a4ce52da2d6116c
rubenChess <ruben_experience@hotmail.com>
2025-01-09 09:08:23 -0600

    Create fruit_store.py
    Decidí no hacer clases, le quité la palabra reservada class, el siguiente cambio será eliminar el argumento self del código

---
+++ fruit_store.py
@@ -0,0 +1,62 @@
+"""
+Ejercicio: Tienda de Frutas
+Vas a escribir un programa que simule la gestión de un pedido en una tienda
+de frutas. El programa debe:
+
+Mostrar el inventario de frutas disponible.
+Permitir al cliente seleccionar las frutas que desea comprar.
+Registrar las cantidades de cada fruta seleccionada.
+Calcular el costo total de la compra.
+Mostrar un resumen detallado del pedido al cliente.
+
+Paso a paso
+1. Crear una función llamada mostrar_inventario
+  - Recibe un diccionario con el inventario de frutas (nombre y precio por
+    kilogramo) como argumento.
+  - Muestra al usuario las frutas disponibles y sus precios.
+2. Crear una función llamada registrar_pedido
+  - Recibe un diccionario con el inventario de frutas.
+  - Permite al cliente seleccionar las frutas que desea comprar.
+  - Devuelve una lista de las frutas seleccionadas por el cliente.
+3. Crear una función llamada registrar_cantidades
+  - Recibe una lista de frutas seleccionadas y el inventario como argumentos.
+  - Pide al cliente la cantidad (en kilogramos) de cada fruta seleccionada.
+  - Devuelve un diccionario con las frutas y sus cantidades.
+4. Crear una función llamada calcular_total
+  - Recibe un diccionario con las frutas seleccionadas y sus cantidades, además
+    del inventario.
+  - Calcula el precio total de la compra.
+  - Devuelve el total y un resumen detallado del pedido.
+5. Crear una función llamada procesar_pedido
+  - Integra todas las funciones anteriores.
+  - Muestra el inventario, permite registrar el pedido, calcula el total y muestra
+    un resumen.
+"""
+  inventario_frutas = {
+   "Piña":{"Inventario": 50 ,"Precio":20},
+   "Naranja":{"Inventario": 100, "Precio": 30}
+  }
+
+  def mostrar_inventario(self):
+    pass
+
+  def registrar_pedido(self):
+    pass
+
+  def registrar_cantidades(self):
+    pass
+
+  def calcular_total(self):
+    pass
+
+  def procesar_pedido(self):
+    pass
+
+# Crear un objeto de tipo fruta
+mi_fruta = Fruit()
+
+# Usar (llamar) los métodos del objeto Fruta
+mi_fruta.mostrar_inventario()
+mi_fruta.registrar_pedido()
+mi_fruta.calcular_total()
+mi_fruta.procesar_pedido()
