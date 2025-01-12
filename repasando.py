62a7b0079fa9d762ad9f3a1a2a5064e2be644629
rubenChess <ruben_experience@hotmail.com>
2025-01-12 16:37:22 -0600

    Update repasando.py
    Decidí realizar unos cambios que me permiten leer mejor el código y comprender lo que hace cada línea, así me sirva de un buen repaso y comprensión en un futuro.
    
    Me falta obtener el promedio de las suma de la lista, en la siguiente versión le agregaré esa función.

--- repasando.py
+++ repasando.py
@@ -2,9 +2,13 @@ lista_numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 def sumar_lista_numeros():
     contador_iteraciones = 0
+    suma_de_cada_numero_lista=0
+    print(f"la lista de numeros es esta: lista_numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] ")
     for tomar_cada_numero_lista in lista_numeros:
         print(f"Voy en la Iteración #{contador_iteraciones}")
         print(f"Estaremos tomando cada número de la lista: {tomar_cada_numero_lista}")
+        suma_de_cada_numero_lista = suma_de_cada_numero_lista + tomar_cada_numero_lista
+        print(f"La suma de la lista en la iteración #{contador_iteraciones} es de = {suma_de_cada_numero_lista}")
         contador_iteraciones = contador_iteraciones + 1
 
 sumar_lista_numeros()
