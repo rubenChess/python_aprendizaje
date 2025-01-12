0971fe9cdb5ab1dcaeae9e6cc954c3f405df69d6
rubenChess <ruben_experience@hotmail.com>
2025-01-12 15:13:06 -0600

    Update repasando.py
    Quise agregar un contador de iteraciones pero me marcó un error algo extraño.
    
    C:\Users\Ruben\Documents\GitHub\python_aprendizaje>python repasando.py
      File "C:\Users\Ruben\Documents\GitHub\python_aprendizaje\repasando.py", line 7
        contador_iteraciones = contador_iteraciones + 1
    TabError: inconsistent use of tabs and spaces in indentation
    
    #Aquí es para agregar el siguiente comando en cmd
    C:\Users\Ruben\Documents\GitHub\python_aprendizaje>

--- repasando.py
+++ repasando.py
@@ -1,6 +1,8 @@
 lista_numeros = [1,2,3,4,5,6,7,8,9,10]
 def sumar_lista_numeros():
+	contador_iteraciones=0
 	for tomar_cada_numero_lista in lista_numeros:
+		print (f"Voy en la Iteración #{contador_iteraciones}")
 		print (f"Estaremos tomando cada numero de la lista: {tomar_cada_numero_lista}")
-	
+        contador_iteraciones = contador_iteraciones + 1
 sumar_lista_numeros()
