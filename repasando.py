66bb5a35cd7ac84fac357b99ee65f607823bd8d3
rubenChess <ruben_experience@hotmail.com>
2025-01-12 15:20:26 -0600

    Update repasando.py
    Solicité apoyo a Flask y honestamente no veo que diferencia hay de mi código al de el , pero da a entender que me faltó identar bien con los espacios, de preferencia 4 espacios correctamente, así como también agregar la línea de código correctamente:
    contador_iteraciones = contador_iteraciones + 1

--- repasando.py
+++ repasando.py
@@ -1,8 +1,10 @@
-lista_numeros = [1,2,3,4,5,6,7,8,9,10]
+lista_numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
 def sumar_lista_numeros():
-	contador_iteraciones=0
-	for tomar_cada_numero_lista in lista_numeros:
-		print (f"Voy en la Iteración #{contador_iteraciones}")
-		print (f"Estaremos tomando cada numero de la lista: {tomar_cada_numero_lista}")
+    contador_iteraciones = 0
+    for tomar_cada_numero_lista in lista_numeros:
+        print(f"Voy en la Iteración #{contador_iteraciones}")
+        print(f"Estaremos tomando cada número de la lista: {tomar_cada_numero_lista}")
         contador_iteraciones = contador_iteraciones + 1
+
 sumar_lista_numeros()
